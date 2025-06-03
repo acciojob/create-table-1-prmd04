8c464560868cd7df7514f2beb80db9e337f174ab
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-03 13:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let table = document.getElementById("sampleTable");
 function insert_Row() {
-    //Write your code here
-  
-  
+    let row = document.createElement("tr");
+    let data = document.createElement("td");
+    let data2 = document.createElement("td");
+
+    data.innerText = `New Cell1`;
+    data2.innerText = `New Cell2`;
+
+    row.appendChild(data);
+    row.appendChild(data2);
+
+    table.childNodes[1].prepend(row);
 }
